54249b6f8d90f5fe5098aca5a2821ba10c411817
Maulana Ibrahim <85508664+maullanaibrahim@users.noreply.github.com>
2022-07-14 06:59:26 +0700

    Upload Models

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -7,8 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Barang extends Model
 {
-    protected $fillable = ['id', 'kode_barang', 'nama_barang', 'satuan', 'harga', 'jenis_barang', 'kategori', 'lead_time', 'created_at', 'update_at'];
-
+    protected $guarded = ['id'];
     public function ppbj_detail()
     {
         return $this->belongsToMany('App\Models\Ppbj_detail');
